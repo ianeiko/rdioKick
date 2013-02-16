8ff52f5be8b4a8021c8bfd3d84ee1531e200a892
jneiku <jneiku@gmail.com>
2013-02-16 15:38:59 -0800

    poll for dom ready

--- js/rdiokick.js
+++ js/rdiokick.js
@@ -1,8 +1,45 @@
 app = {
 	init: function(){
-		// var tabsDOM = document.querySelectorAll('.tabs')[0],
-		// 	iframe = document.querySelectorAll('iframe')[0];
+		var poll = function(){
+			try{
+				var tabContainer = document.querySelectorAll('.tabs')[0]
+				
+				if(tabContainer.innerHTML){ // poll until DOM is ready
+					app.createTabs(tabContainer);
+				}
+			}catch(e){
+				setTimeout(function(){
+					poll();
+				}, 1000);
+			}
+		}
+		poll();
+	},
+	createTabs: function(tabContainer){
+		var lastTab = document.querySelectorAll('.tabs .tab.last')[0],
+			newTab = document.createElement('div'),
+			newLink = document.createElement('a');
+
+		// modify last tab
+		if(lastTab){
+			lastTab.classList.remove('last');	
+		}
 		
+		// create and append new tab
+		newLink.innerText = 'Concerts';
+		newLink.href = '#';
+		newTab.className = 'tab last';
+		newTab.appendChild(newLink);
+		tabContainer.appendChild(newTab);
+		
+		// listen for clicks
+		newLink.addEventListener('click', function(e){
+			app.fetchConcertData();
+			return false;
+		});
+	},
+	fetchConcertData: function(){
+		// use document.location.pathname to get artist's name
 	}
 }
 app.init();
